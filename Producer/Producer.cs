2aa0fd3570daa1b42973e10d80950f2a0eb36a0f
Mingson Zheng <MingsonZheng@outlook.com>
2021-05-17 19:39:25 +0800

    添加项目文件。

---
+++ Producer/Producer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+            // 所有发送到Direct Exchange的消息被转发到具有指定RouteKey的Queue
+            // 所有发送到Fanout Exchange的消息都会被转发到与该Exchange 绑定(Binding)的所有Queue上
+            // 所有发送到Topic Exchange的消息被转发到能和Topic匹配的Queue上
+
+            //var directExchange = "directExchange";
+            //var directQueue = "directQueue";
+            //var directKey = "directKey";
+
+            //var fanoutExchange = "fanoutExchange";
+            //var fanoutQueue1 = "fanoutQueue1";
+            //var fanoutQueue2 = "fanoutQueue2";
+            //var fanoutKey = "";// Fanout Exchange 不需要处理RouteKey
+
+            var topicExchange = "topicExchange";
+            var topicQueue = "topicQueue";
+            var topicKey = "topicKey.*";// 符号“#”匹配一个或多个词，符号“*”匹配不多不少一个词
+
+            //创建连接工厂
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",//用户名
+                Password = "guest",//密码
+                HostName = "127.0.0.1"//rabbitmq ip
+            };
+
+            //创建连接
+            var connection = factory.CreateConnection();
+
+            //创建通道
+            var channel = connection.CreateModel();
+
+            //定义交换机
+            //channel.ExchangeDeclare(directExchange, ExchangeType.Direct, false, false, null);// Direct模式
+            //channel.ExchangeDeclare(fanoutExchange, ExchangeType.Fanout, false, false, null);// Fanout模式
+            channel.ExchangeDeclare(topicExchange, ExchangeType.Topic, false, false, null);// Topic模式
+
+            //声明队列
+            //channel.QueueDeclare("hello", false, false, false, null);// hello
+            //channel.QueueDeclare(directQueue, false, false, false, null);// Direct模式
+            //channel.QueueDeclare(fanoutQueue1, false, false, false, null);// Fanout模式
+            //channel.QueueDeclare(fanoutQueue2, false, false, false, null);// Fanout模式
+            channel.QueueDeclare(topicQueue, false, false, false, null);// Topic模式
+
+            //将队列绑定到交换机
+            //channel.QueueBind(directQueue, directExchange, directKey, null);// Direct模式
+            //channel.QueueBind(fanoutQueue1, fanoutExchange, fanoutKey, null);// Fanout模式
+            //channel.QueueBind(fanoutQueue2, fanoutExchange, fanoutKey, null);// Fanout模式
+            channel.QueueBind(topicQueue, topicExchange, topicKey, null);// Topic模式
+
+            Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入exit退出！");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                //发布消息
+                //channel.BasicPublish("", "hello", null, sendBytes);// hello
+                //channel.BasicPublish(directExchange, directKey, null, sendBytes);// Direct模式
+                //channel.BasicPublish(fanoutExchange, fanoutKey, null, sendBytes);// Fanout模式
+                channel.BasicPublish(topicExchange, "topicKey.one", null, sendBytes);// Topic模式
+
+            } while (input.Trim().ToLower() != "exit");
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ consumer/Consumer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+			//创建连接工厂
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",//用户名
+                Password = "guest",//密码
+                HostName = "127.0.0.1"//rabbitmq ip
+            };
+
+            //创建连接
+            var connection = factory.CreateConnection();
+            var connection2 = factory.CreateConnection();
+            //创建通道
+            var channel = connection.CreateModel();
+            var channel2 = connection2.CreateModel();
+
+            //事件基本消费者
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            EventingBasicConsumer consumer2 = new EventingBasicConsumer(channel2);
+
+            //接收到消息事件
+            consumer.Received += (ch, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine($"收到消息： {message}");
+
+                //Console.WriteLine($"收到该消息[{ea.DeliveryTag}] 延迟2s发送回执");
+                //Thread.Sleep(2000);
+                //确认该消息已被消费
+                channel.BasicAck(ea.DeliveryTag, false);
+                Console.WriteLine($"已发送回执[{ea.DeliveryTag}]");
+            };
+            consumer2.Received += (ch, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine($"收到消息： {message}");
+
+                //确认该消息已被消费
+                channel.BasicAck(ea.DeliveryTag, false);
+                Console.WriteLine($"已发送回执[{ea.DeliveryTag}]");
+            };
+
+            //启动消费者 设置为手动应答消息
+            //channel.BasicConsume("hello", false, consumer);// hello
+            //channel.BasicConsume("directQueue", false, consumer);// Direct模式
+            //channel.BasicConsume("fanoutQueue1", false, consumer);// Fanout模式
+            //channel.BasicConsume("fanoutQueue2", false, consumer2);// Fanout模式
+            channel.BasicConsume("topicQueue", false, consumer);// Topic模式
+
+            Console.WriteLine("消费者已启动");
+            Console.ReadKey();
+            channel.Dispose();
+            connection.Close();
+		}
+    }
+}
