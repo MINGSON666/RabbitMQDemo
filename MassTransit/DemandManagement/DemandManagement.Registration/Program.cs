2d1d8eeb473500b942abc450ff5791433f71644c
郑子铭 <mingsonzheng@outlook.com>
2021-05-27 10:05:22 +0800

    Exceptions

--- MassTransit/DemandManagement/DemandManagement.Registration/Program.cs
+++ MassTransit/DemandManagement/DemandManagement.Registration/Program.cs
@@ -2,6 +2,8 @@
 using GreenPipes;
 using MassTransit;
 using System;
+using System.Data;
+using MassTransit.Pipeline.Filters;
 
 namespace DemandManagement.Registration
 {
@@ -25,9 +27,48 @@ namespace DemandManagement.Registration
                             cb.ActiveThreshold = 10;
                             cb.ResetInterval = TimeSpan.FromMinutes(5);
                         });
-                     
+
+                        //// 为多种异常类型指定过滤器
+                        //e.UseMessageRetry(r =>
+                        //{
+                        //    r.Handle<ArgumentNullException>();
+                        //    r.Ignore(typeof(InvalidOperationException), typeof(InvalidCastException));
+                        //    r.Ignore<ArgumentException>(t => t.ParamName == "orderTotal");
+                        //});
+
+                        //// 为单个端点指定多个重试策略
+                        //e.UseMessageRetry(r =>
+                        //{
+                        //    r.Immediate(5);
+                        //    r.Handle<DataException>(x => x.Message.Contains("SQL"));
+                        //});
+                        //e.Consumer<RegisterDemandCommandConsumer>(c => c.UseMessageRetry(r =>
+                        //    {
+                        //        r.Interval(10, TimeSpan.FromMilliseconds(200));
+                        //        r.Ignore<ArgumentNullException>();
+                        //        r.Ignore<DataException>(x => x.Message.Contains("SQL"));
+                        //    })
+                        //);
+
+                        //// 如果前5次立即重试失败（数据库确实非常崩溃），则消息将在5、15和30分钟后再重试3次
+                        //e.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(30)));
+                        //e.UseMessageRetry(r => r.Immediate(5));
+                        //// MassTransit提供了一个发件箱来缓冲这些消息
+                        //e.UseInMemoryOutbox();
+
+                        //// 错误管道默认过滤器
+                        //e.ConfigureError(x =>
+                        //{
+                        //    x.UseFilter(new GenerateFaultFilter());
+                        //    x.UseFilter(new ErrorTransportFilter());
+                        //});
+
+                        //// 死信管道默认过滤器
+                        //e.ConfigureDeadLetter(x =>
+                        //{
+                        //    x.UseFilter(new DeadLetterTransportFilter());
+                        //});
                     });
-              
             });
 
             bus.StartAsync();
--- MassTransit/DemandManagement/DemandManagement.Registration/RegisterDemandCommandConsumer.cs
+++ MassTransit/DemandManagement/DemandManagement.Registration/RegisterDemandCommandConsumer.cs
@@ -13,6 +13,9 @@ namespace DemandManagement.Registration
     {
         public Task Consume(ConsumeContext<IRegisterDemandCommand> context)
         {
+            //// 在默认的总线配置下，异常由传输中的中间件捕获（ErrorTransportFilter确切地说是），并且消息被移至_error队列（由接收端点队列名称作为前缀）
+            //throw new Exception("Very bad things happened");
+
             var message = context.Message;
             var guid = Guid.NewGuid();
             Console.WriteLine($"Demand successfully  registered. Subject:{message.Subject}, Description:{message.Description}, Id:{guid}");
